7f2154ff70889786301f1531ac3b3bf17e206013
shashank24 <shashankanakal2004@gmail.com>
2025-03-16 23:21:06 +0530

    Seventh Learning Class of LangChain

---
+++ 7.Chains/simple_chain.py
@@ -0,0 +1,26 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+prompt = PromptTemplate(
+    template='Generate 5 interesting facts about {topic}',
+    input_variables=['topic']
+)
+
+
+model = ChatGoogleGenerativeAI(model ='gemini-1.5-pro')
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke({'topic':'cricket'})
+
+print(result)
+
+
+#Helps to Visualisation of chain shows the step by step process
+chain.get_graph().print_ascii()
